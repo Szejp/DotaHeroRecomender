53346e1311de78b21e434d42df59e5ca4891250d
Szejp <janqczmowski@gmail.com>
2016-12-10 20:00:40 +0100

    Created functionality for heroes adding and almost counters adding

--- DotaHeroRecommender/Helper/HeroesPageReader.cs
+++ DotaHeroRecommender/Helper/HeroesPageReader.cs
@@ -16,6 +16,7 @@ namespace DotaHeroRecommender.Helper
         HtmlDocument document;
         string pageString = "https://mobacounter.com/dota";
         string heroesGridXPath = "//div[@class='clearfix']//a[contains(@href,'dota/')]/@href";
+        string countersXPath = "//*[text()[contains(.,'weak against')]]/..//ul//li//h3/a";
         public string dataContent { get; private set; }
 
         List<string> heroesUrls;
@@ -33,12 +34,58 @@ namespace DotaHeroRecommender.Helper
             string source = webClient.DownloadString(pageString);
             document.LoadHtml(source);
 
-            var grid = document.DocumentNode.SelectNodes(heroesGridXPath);
+            var heroesGrid = document.DocumentNode.SelectNodes(heroesGridXPath);
 
-            foreach(var a in grid)
+            foreach(var a in heroesGrid)
             {
-                heroesUrls.Add(a.Attributes[0].Value);
+                var heroPath = a.Attributes[0].Value.Replace("/dota", "");
+                heroesUrls.Add(heroPath);
             }
+        }
+
+        public List<string> GetHeroCounterNamesByPath(string path)
+        {
+            string source = webClient.DownloadString(pageString + path);
+            document.LoadHtml(source);
+            return GenerateCountersNameList();
+        }
+
+        public List<string> GetHeroCounterNamesByNames(string name)
+        {
+            string source = webClient.DownloadString(pageString + "/" + name);
+            document.LoadHtml(source);
+            return GenerateCountersNameList();
+        }
+
+        private List<string> GenerateCountersNameList()
+        {
+            var heroCountersNodes = document.DocumentNode.SelectNodes(countersXPath);
+            List<string> heroCountersNames = new List<string>();
+
+            foreach (var node in heroCountersNodes)
+            {
+                heroCountersNames.Add(node.InnerText);
+            }
+
+            return heroCountersNames;
+        }
+
+        public void GetCounters()
+        {
+            foreach(var p in heroesUrls)
+            {
+                GetHeroCounterNamesByPath(p);
+            }
+        }
+
+        public List<string> GetHeroNames()
+        {
+            if (heroesUrls == null)
+            {
+                GetHeroesUrlList();
+            }
+
+            return heroesUrls.Select(p => p.Replace("/", "")).ToList();
         }
     }
 }
--- DotaHeroRecommender/Program.cs
+++ DotaHeroRecommender/Program.cs
@@ -15,10 +15,68 @@ namespace DotaHeroRecommender
         {
             HeroesPageReader reader = new HeroesPageReader();
             reader.GetHeroesUrlList();
+
+            //AddCounters();
+
+            
+            
+            CheckDb();     
+        }
+
+        private static void RemoveHero(string name)
+        {
+            using (var db = new DotaHeroContext())
+            {
+
+            }
+        }
+
+        private static void AddHeroes()
+        {
+            HeroesPageReader reader = new HeroesPageReader();
+            reader.GetHeroesUrlList();
+            var names = reader.GetHeroNames();
+
+            using (var db = new DotaHeroContext())
+            {
+                foreach (var n in names)
+                {
+                    Hero hero = new Hero { Name = n };
+                    db.Heroes.Add(hero);
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        private static void AddCounters()
+        {
+            HeroesPageReader reader = new HeroesPageReader();
+            reader.GetHeroesUrlList();          
+
+            using (var db = new DotaHeroContext())
+            {
+
+                var heroes = db.Heroes;
+
+                foreach (var h in heroes)
+                {
+                    var counters = reader.GetHeroCounterNamesByNames(h.Name);
+                    var hero = db.Heroes.SingleOrDefault(p => p.Name == h.Name);
+                    hero.Counters.Counter1 = db.Heroes.SingleOrDefault(r => r.Name == counters[0]);
+                    hero.Counters.Counter2 = db.Heroes.SingleOrDefault(r => r.Name == counters[1]);
+                    hero.Counters.Counter3 = db.Heroes.SingleOrDefault(r => r.Name == counters[2]);
+                    hero.Counters.Counter4 = db.Heroes.SingleOrDefault(r => r.Name == counters[3]);
+                    hero.Counters.Counter5 = db.Heroes.SingleOrDefault(r => r.Name == counters[4]);
+                    db.Heroes.Add(hero);
+                }
+
+                db.SaveChanges();
+            }
         }
 
 
-        private void CheckDb()
+        private static void CheckDb()
         {
             using (var db = new DotaHeroContext())
             {
@@ -30,7 +88,9 @@ namespace DotaHeroRecommender
                 Console.WriteLine("All heroes in the database:");
                 foreach (var item in query)
                 {
-                    Console.WriteLine(item.Name);
+                    Console.WriteLine(item.Name + item.Counters.Counter1.Name +
+                        item.Counters.Counter2.Name + item.Counters.Counter3.Name
+                        + item.Counters.Counter4.Name + item.Counters.Counter5.Name);
                 }
 
                 Console.WriteLine("Press any key to exit...");
